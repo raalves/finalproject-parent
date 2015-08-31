7839a16d5e2b08be3045f0e43235529a44f66e57
babela86 <lourencorafaela@gmail.com>
2015-08-31 20:57:49 +0100

    tabs candidate

---
+++ finalproject-web/src/main/java/pt/uc/dei/aor/pf/rafaelaricardo/CandidateCandidaturesBean.java
@@ -0,0 +1,32 @@
+package pt.uc.dei.aor.pf.rafaelaricardo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class CandidateCandidaturesBean implements Serializable {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<Integer> candidaturesCandidate;
+
+	public CandidateCandidaturesBean() {
+
+		candidaturesCandidate = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			candidaturesCandidate.add(i);
+		}
+	}
+
+	public List<Integer> getCandidaturesCandidate() {
+		return candidaturesCandidate;
+	}
+
+}
