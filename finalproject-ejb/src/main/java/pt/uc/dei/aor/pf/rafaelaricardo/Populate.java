9eb1cefe492e2a46c2bc871b8f824e17dc269180
babela86 <lourencorafaela@gmail.com>
2015-09-23 12:31:26 +0100

    mais elementos populate

--- finalproject-ejb/src/main/java/pt/uc/dei/aor/pf/rafaelaricardo/Populate.java
+++ finalproject-ejb/src/main/java/pt/uc/dei/aor/pf/rafaelaricardo/Populate.java
@@ -52,7 +52,7 @@ public class Populate implements Serializable {
 	void populando() throws ParseException {
 
 		log.info("Populating the DB");
-
+		// Creating Roles
 		ArrayList<RoleEntity> rAdmin = new ArrayList<RoleEntity>();
 		rAdmin.add(new RoleEntity(Role.ADMIN));
 		ArrayList<RoleEntity> rInterv = new ArrayList<RoleEntity>();
@@ -68,7 +68,7 @@ public class Populate implements Serializable {
 		// rAdminMan.add(new RoleEntity(Role.INTERVIEWER));
 
 		RoleEntity rCAnd = new RoleEntity(Role.CANDIDATE);
-
+		// Creating Users
 		UserEntity[] users = {
 				new UserEntity("admin", "das couves", "admin@gmail.com",
 						encPass.encrypt("123"), rAdmin),
@@ -82,12 +82,14 @@ public class Populate implements Serializable {
 		// new UserEntity("adminMan", "das couves", "teste2@gmail.com",
 		// encPass.encrypt("abc"), rAdminMan) };
 
+		// Creating Posiitions
+		// Position1
 		DescriptionPosition dp = new DescriptionPosition();
 		dp.setDescription("Working for us is like nothing on earth. Every day, our teams across the globe challenge the limits of human achievement, engineering solutions for our planet and beyond.&lt;br/&gt; Our astronomically talented engineers build rock-solid software for leading industries’ most critical applications. Now, we’re looking for talented Technical Managers for Embedded Systems to join our Systems and Software Engineering team in breaching the frontiers of space, aerospace and defence."
 				+ "Across international projects, you’ll be working on some of the most critical software applications developing today, as part of a global team with more than 15 years’ experience working with embedded software and systems. '&lt;br/&gt;'"
 				+ "Sound like you? Then it’s time to challenge your limits. It’s time to give your career the rocket boost it deserves. It’s time you joined CRITICAL Software!"
 				+ "");
-		dp.setKeyResponsabilities("<p>Undertaking software development for embedded systems</p><p>Performing Verification & Validation testing of embedded systems</p><p>Leading a team through your technical expertise (task allocation, status reporting, and people management)keyResponsabilities</p>");
+		dp.setKeyResponsabilities("<p>Undertaking software development for embedded systems</p><p>Performing Verification & Validation testing of embedded systems</p><p>Leading a team through your technical expertise (task allocation, status reporting, and people management)</p>");
 		dp.setDesiredQualifications("<p>Knowledge of embedded software development in microcontrollers</p><p>Knowledge of safety-critical standards (for example, ECSS, DO-178B/C, ISO 26262, IEC 61508)</p><p>Knowledge of system development using model-based development techniques and tools (for example, SCADE and MATLAB)</p>");
 		dp.setRequiredQualifications("<p>More than 7 years’ experience working in software development and embedded systems engineering</p><p>Deep knowledge of different Operating Systems, particularly the concepts of real-time and embedded systems, computer architectures and programming languages (C/C++, Ada95)</p><p>Full-lifecycle software development experience, from initial requirements elicitation to design, coding, testing, documentation, implementation, integration and training</p><p>Teamwork and technical project management experience</p><p>Strong leadership, problem-solving and communication skills</p><p>Proficiency in English</p><p>Good organisational skills</p><p>Availability to travel, mainly Europe (United Kingdom, Germany, France)</p>");
 
@@ -104,7 +106,6 @@ public class Populate implements Serializable {
 		sourcesPos1.add(Source.GLASSDOOR);
 		sourcesPos1.add(Source.LINKEDIN);
 		List<Location> localPos1 = new ArrayList<Location>();
-
 		localPos1.add(Location.COIMBRA);
 		localPos1.add(Location.OPORTO);
 
@@ -125,11 +126,74 @@ public class Populate implements Serializable {
 		position1.setGuide(generalGuide);
 		position1.setManager(users[0]);
 
+		// Position2
+		DescriptionPosition dp2 = new DescriptionPosition();
+		dp2.setDescription("Working for us is like nothing on earth! Every day, our teams challenge the limits of human achievement, engineering solutions for our planet and beyond. Our astronomically talented engineers build rock-solid software for leading industries’ most critical applications."
+				+ "Now, we’re looking for a talented .Net Software Engineer to join our team."
+				+ "This is a great opportunity to join a high tech international company that genuinely cares about its employees and has recently become “Employer of the Year” in local business awards.  The business has customers in the aerospace, energy and defence sectors that demand high integrity and complex systems to meet their challenging requirements."
+				+ "It’s time to challenge your limits. It’s time to give your career the rocket boost it deserves. It’s time you joined CRITICAL Software!.");
+		dp2.setKeyResponsabilities("The successful candidate will be expected to undertake a variety of duties. These include, but are not limited to:"
+				+ "Performing software engineering tasks to a high standard, including software requirements, design, development and testing"
+				+ "Configuration management"
+				+ "Development and maintenance of data visualisation software in web technologies"
+				+ "Development and maintenance of data loading processes (ETL)"
+				+ "Business Intelligence development from ETL through to report development"
+				+ "Statistical and mathematical modelling of data"
+				+ "You will need to be very client focused and feel comfortable in understanding and acting on customer needs. You will also be expected to contribute to bid work.");
+		dp2.setDesiredQualifications("Microsoft Certified Developer"
+				+ "Exposure to agile development"
+				+ "Experience of dealing with customers and staff"
+				+ "Web development skills (e.g. AngularJs, JQuery, HTML 5, CSS, JavaScript, LESS, SASS & Knockout)"
+				+ "Experience with ORM (e.g. Entity Framework and NHibernate)"
+				+ "Database development and SQL"
+				+ "Experience within the aerospace, defence, energy or transport sectors"
+				+ "Other programming languages");
+		dp2.setRequiredQualifications("Educated to degree level in a computing or mathematically based subject achieving a 2:1 or higher"
+				+ "Experience and ability to demonstrate knowledge with developing web applications using the Microsoft Stack: Visual Studio 2012+, ASP.NET, C# and SQL Server 2008+"
+				+ "Strong object-oriented development skills"
+				+ "Design experience using UML and design patterns"
+				+ "Testing at module and system level (creating test scripts and procedures)"
+				+ "Configuration management");
+
+		GuideEntity generalGuide2 = new GuideEntity();
+		generalGuide2.setGuideTitle("guide position2");
+		generalGuide2.setGuideDate(ft.parse("2015-06-01"));
+		generalGuide2.setFilePath("qqcoisa2");
+		generalGuide2.setAuthor(users[0]);
+
+		List<Source> sourcesPos2 = new ArrayList<Source>();
+		sourcesPos2.add(Source.CRITICAL_SOFTWARE_WEBSITE);
+		sourcesPos2.add(Source.LINKEDIN);
+
+		List<Location> localPos2 = new ArrayList<Location>();
+		localPos2.add(Location.COIMBRA);
+		localPos2.add(Location.CLIENT);
+		localPos2.add(Location.OPORTO);
+
+		PositionEntity position2 = new PositionEntity();
+		position2.setAdminCreator(users[0]);
+		position2.setTitle(".NET SOFTWARE ENGINEER");
+		position2.setLocation(localPos2);
+
+		position2.setPositionStatus(PositionStatus.OPEN);
+		position2.setQuantity(1);
+		position2.setCompany("Critical Software");
+		position2.setTechnicalArea(TechnicalArea.NET_DEVELOPMENT);
+		position2.setDescriptionPosition(dp2);
+		position2.setSource(sourcesPos2);
+		position2.setOpenningDate(ft.parse("2015-09-01"));
+		position2.setClosingDate(ft.parse("2015-11-31"));
+		position2.setSla(8);
+		position2.setGuide(generalGuide2);
+		position2.setManager(users[0]);
+
+		// Create Candidate
+		// Candidate1
 		CandidateEntity candidate1 = new CandidateEntity();
-		candidate1.setFirstName("Rafaela	");
+		candidate1.setFirstName("Rafaela");
 		candidate1.setLastName("Lourenço");
 		candidate1.setBirthDate(ft.parse("1986-04-30"));
-		candidate1.setEmail("lourencorafaela@gmail.com");
+		candidate1.setEmail("rafaela@gmail.com");
 		candidate1.setPassword(encPass.encrypt("456"));
 		candidate1.setMobilePhone((long) 912345678);
 		candidate1.setPhone((long) 274111111);
@@ -144,7 +208,7 @@ public class Populate implements Serializable {
 		// candidate1.setCandidatures(candidatures);
 
 		List<Source> sourceCand1 = new ArrayList<Source>();
-		sourceCand1.add(Source.FACEBOOK);
+		sourceCand1.add(sourcesPos1.get(1));
 
 		ArrayList<CandidatureEntity> candidatures = new ArrayList<CandidatureEntity>();
 		CandidatureEntity candidature1 = new CandidatureEntity("qqcoisaCV",
@@ -155,6 +219,40 @@ public class Populate implements Serializable {
 		candidature1.setCandidate(candidate1);
 
 		candidatures.add(candidature1);
+
+		// Candidate2
+		CandidateEntity candidate2 = new CandidateEntity();
+		candidate2.setFirstName("Ricardo");
+		candidate2.setLastName("Alves");
+		candidate2.setBirthDate(ft.parse("1983-03-24"));
+		candidate2.setEmail("ricardo@gmail.com");
+		candidate2.setPassword(encPass.encrypt("456"));
+		candidate2.setMobilePhone((long) 912345678);
+		candidate2.setPhone((long) 274111111);
+		candidate2.setAddress("algures na serta");
+		candidate2.setCity("Sertã");
+		candidate2.setCountry("Portugal");
+		candidate2.setCourse("Eng Bioquimica");
+		candidate2.setSchool("FCUL");
+		candidate2.setCvPath("qqcoisa");
+		candidate2.setRole(rCAnd);
+		// candidate1.setCandidatures(candidatures);
+
+		List<Source> sourceCand2 = new ArrayList<Source>();
+		// sourceCand2.add(sourcesPos1.get(0));
+
+		// ArrayList<CandidatureEntity> candidatures = new
+		// ArrayList<CandidatureEntity>();
+		// CandidatureEntity candidature = new CandidatureEntity("qqcoisaCV",
+		// "qqCoisaCoverLetter", ft.parse("2015-08-31"),
+		// CandidatureStatus.SUBMITTED);
+		// candidature1.setPosition(position1);
+		// candidature1.setPublicSource(sourceCand1);
+		// candidature1.setCandidate(candidate1);
+
+		// candidatures.add(candidature1);
+
+		// Persisting all creations
 
 		// for (RoleEntity rl : allRoles)
 		// em.persist(rl);
@@ -175,6 +273,9 @@ public class Populate implements Serializable {
 		em.persist(generalGuide);
 		em.persist(position1);
 		em.persist(candidate1);
+		em.persist(generalGuide2);
+		em.persist(position2);
+		em.persist(candidate2);
 		for (CandidatureEntity c : candidatures)
 			em.persist(c);
 
